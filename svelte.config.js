c209c93fc043ef0129db642c0c93889b2afaad7e
FlavioSolon <solondutra73@gmail.com>
2026-03-06 17:20:43 -0400

    ajustes de deploy

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		output: {
+			preloadStrategy: 'empty' // Disable unused preload warnings in Chrome
+		},
+		version: {
+			pollInterval: 5 * 60 * 1000 // Poll for new deploys every 5 minutes
+		}
 	}
 };
 
